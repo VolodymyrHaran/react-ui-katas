9ef141441f984705780af4ab76f94da3643c4d3b
Volodymyr Haran <aidenhowlett35@gmail.com>
2026-03-04 22:38:25 +0100

    feat: add counter component and base project structure

---
+++ react-ui-katas/src/components/Counter.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
+}
